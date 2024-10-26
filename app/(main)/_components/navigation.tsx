bbd8b9f3c946042505c1a9de7bc2046b24b1b1a0
Harsh Tiwari <harsh.official1802@gmail.com>
2024-10-26 23:32:48 +0530

    "Fix: Responsive SideBar"

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 import {
   ChevronsLeft,
-  Menu,
+  MenuIcon,
   PlusCircle,
   Search,
   Settings2,
@@ -14,6 +14,7 @@ import { toast } from "sonner";
 import UserItem from "./userItem";
 import Item from "./item";
 import DocumentList from "./documentList";
+import { cn } from "@/lib/utils";
 
 function Navigation() {
   const create = useMutation(api.documents.create);
@@ -32,6 +33,51 @@ function Navigation() {
       success: "New note created",
       error: "Failed to create new note",
     });
+  };
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [isMobile, pathName]);
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    let newWidth = e.clientX;
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% -  ${newWidth}px)`,
+      );
+    }
+  };
+
+  const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
   };
 
   const resetWidth = () => {
@@ -96,15 +142,25 @@ function Navigation() {
         <div className="mt-4">
           <DocumentList />
         </div>
+        <div
+          onMouseDown={handleMouseDown}
+          onClick={resetWidth}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+        />
       </aside>
 
       <div
         ref={navbarRef}
-        className="absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]"
+        className={cn(
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          (isMobile || isCollapsed) && "left-0 w-full", // Adjust for mobile or collapsed state
+        )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
+          {/* Show MenuIcon only when sidebar is collapsed */}
           {(isMobile || isCollapsed) && (
-            <Menu
+            <MenuIcon
               role="button"
               className="h-6 w-6 text-muted-foreground"
               onClick={resetWidth}
