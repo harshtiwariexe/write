541ac2e05ec5aef3ed2948cc120759dffeca92cc
Harsh Tiwari <harsh.official1802@gmail.com>
2024-11-09 21:06:22 +0530

    "Feature: Menu For Banner ti delete note from inside the note"

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import Title from "./title";
 import Banner from "./banner";
+import Menu from "./menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -45,8 +46,11 @@ export default function NavBar({ isCollapsed, onResetWidth }: NavbarProps) {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -65,6 +69,9 @@ export default function NavBar({ isCollapsed, onResetWidth }: NavbarProps) {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
